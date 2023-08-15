c379f179ee75488b37cc244ab8cc1bc5c1d56a84
tylersmith2614 <tylersmith2614@gmail.com>
2023-08-15 13:34:24 -0600

    updated readme and set cache: bounded

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -5,19 +5,17 @@ const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
 
-
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
+  cache: "bounded", // Set cache to bounded
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
@@ -26,7 +24,6 @@ if (process.env.NODE_ENV === "production") {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -41,6 +38,5 @@ const startApolloServer = async (typeDefs, resolvers) => {
     });
   });
 };
-
 
 startApolloServer(typeDefs, resolvers);
